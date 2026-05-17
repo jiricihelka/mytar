40192c2a3efe7bbb28199f360a3735fab2406525
Jiří Cihelka <dev+git@jiric.cz>
2026-05-17 15:17:48 +0200

    Read always full blocks

--- mytar.c
+++ mytar.c
@@ -158,7 +158,7 @@ void extract_file(const char *filename, size_t file_size, FILE *archive) {
     size_t bytes_remaining = file_size;
     while (bytes_remaining > 0) {
         size_t bytes_to_read = bytes_remaining < sizeof(buffer) ? bytes_remaining : sizeof(buffer);
-        size_t bytes_read = fread(buffer, 1, bytes_to_read, archive);
+        size_t bytes_read = fread(buffer, 1, sizeof(buffer), archive);
         if (bytes_read < bytes_to_read) {
             fprintf(stderr, "mytar: Unexpected EOF in archive\n");
             fclose(output);
